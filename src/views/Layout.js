2a839d96c7538af1b3b9c035b0ba9b0961508172
Thomas Yager-Madden <10351894+tym-oao@users.noreply.github.com>
2018-06-08 13:39:02 -0500

    Add Bulma styles

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -3,20 +3,24 @@ var m = require("mithril")
 
 module.exports = {
     view: function(vnode) {
-        return m("main.layout", [
-            m("img[style='text-align: center;']", {
+        return m("main.section.container", [
+            m(".columns.is-centered",
+              m(".column.is-narrow"),
+                m("img.is-128x128", {
                 "src": "https://dl.dropboxusercontent.com/s/71rf6yosekslnog/skate.png",
-            }),
-            m("h1", "Baby Experimental Nerium Client" 
+            })),
+            m("h1.title", "Baby Experimental Nerium Client" 
             ),
-            m("p",
+            m("p.subtitle",
                 [ "Fetches data from a ",
                 m("a[href='https://nerium.kzfrb3.pw/v1/test/?greeting=sup']", "Nerium"),
                 " instance running on my personal GCP account"]
             ),
-            m("section", vnode.children),
-            m("p", {"style": "text-align: right;"}, m("a[href='https://github.com/tym-oao/nerium-baby-client']", "Source")
-            )
+            m("section.section", vnode.children),
+            m("footer",
+                m("p.has-text-right", 
+                    m("a[href='https://github.com/tym-oao/nerium-baby-client']", "Source")
+            ))
         ])
     }
 }
