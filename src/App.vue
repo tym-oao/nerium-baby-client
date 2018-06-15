b8873b4c9d41119d64053c2d31a016123f3ad6ac
Thomas Yager-Madden <10351894+tym-oao@users.noreply.github.com>
2018-06-14 19:17:50 -0500

    migrate to Vue.js

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id='app'>
+    <div class='section form columns is-centered'>
+      <div class='column is-4'>
+          <form ref='greeting-form' v-on:submit.prevent='updateGreeting'>
+              <input type='text' placeholder='quux' v-model='newGreeting' class='form-field is-size-4'></input>
+              <button ref='submit' class='button is-primary is-tooltip-info tooltip' data-tooltip='Change greeting query parameter'>Go</button>
+          </form>
+      </div>
+    </div>
+    <div class='result'>
+      <div class='row' v-for='row in resultSet'>
+          <p v-for='(col, c) in row'><strong>{{ c + ': ' }}</strong>{{ col }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    load() {
+    var req = new XMLHttpRequest()
+    var url ='https://nerium.kzfrb3.pw/v1/test/?greeting=' + this.greeting
+    console.log(url)
+    req.open('GET', url, true)
+    req.onload = () => {
+      this.resultSet = JSON.parse(req.responseText).data
+      console.log(this.resultSet)
+    }
+    req.send()
+    },
+    updateGreeting() {
+      this.greeting = this.newGreeting
+      this.load()
+      console.log(this.greeting)
+    }
+  },
+  data () {
+    return {
+      resultSet: [],
+      greeting: 'sup',
+      newGreeting: '',
+    }
+  },
+  mounted() {
+    this.load()
+  }
+}
+
+
+</script>
